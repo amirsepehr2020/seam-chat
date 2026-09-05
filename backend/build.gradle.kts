47c32209ba7a1933df0c4350179cad2f5cb26f59
amirsepehr2020 <sepehr2sodoury@gmail.com>
2026-09-05 15:50:38 +0330

    feat: configure self-hosted backend

---
+++ backend/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+    kotlin("plugin.serialization") version "2.2.20"
+    application
+}
+
+group = "amir.sepehr"
+version = "1.0.0"
+
+application { mainClass.set("amir.sepehr.seamchat.ApplicationKt") }
+
+repositories { mavenCentral() }
+
+val ktorVersion = "3.3.0"
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-websockets-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors-jvm:$ktorVersion")
+    implementation("com.zaxxer:HikariCP:7.0.2")
+    implementation("org.postgresql:postgresql:42.7.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+    testImplementation(kotlin("test"))
+}
+
+kotlin { jvmToolchain(17) }
