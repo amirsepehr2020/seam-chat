24483c7f179e48817cbf87a73ec91e61adbde2fb
amirsepehr2020 <sepehr2sodoury@gmail.com>
2026-09-08 19:01:32 +0330

    chore: clear repository

--- backend/settings.gradle.kts
+++
@@ -1,3 +0,0 @@
-pluginManagement { repositories { gradlePluginPortal(); mavenCentral() } }
-dependencyResolutionManagement { repositories { mavenCentral() } }
-rootProject.name = "seam-chat-backend"
